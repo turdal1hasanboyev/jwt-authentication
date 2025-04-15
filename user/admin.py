40c79c5dbbd56c1594e4a7d57ef6eef041a61681
Turdali <turdalihasanboyev70@gmail.com>
2025-04-15 16:26:15 +0500

    `Added custom user model and admin interface`

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    ordering = ('email',)
+    search_fields = ('email',)
+    list_display = (
+        'id',
+        'email',
+        'is_active',
+        'is_superuser',
+        'is_staff',
+        'last_login',
+        'date_joined',
+    )
+    list_filter = (
+        'is_active',
+        'is_superuser',
+        'is_staff',
+    )
+    fieldsets = (
+        (None, {
+            'fields': ('email', 'password',),
+        }),
+        ("Permissions", {
+            'fields': ('is_superuser', 'is_staff', 'is_active',),
+        }),
+        ("Important Dates", {
+            'fields': ('date_joined', 'last_login',),
+        }),
+    )
+    add_fieldsets = (
+        ('Create Super User', {
+            'fields': ('email', 'password1', 'password2',),
+        }),
+        ("Permissions", {
+            'fields': ('is_superuser', 'is_staff', 'is_active',),
+        }),
+    )
--- user/models.py
+++ user/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager
 
-# Create your models here.
+
+class CustomUserManager(UserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        user = self.create_user(email, password=password, **extra_fields)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class CustomUser(AbstractUser):
+    first_name = None
+    last_name = None
+    username = None
+    email = models.EmailField(unique=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f"{self.email}"
